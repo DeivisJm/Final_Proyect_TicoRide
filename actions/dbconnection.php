18268752c51151613d01f9f6ad64b0df3525b520
DeivisJm <deivisjm1205@gmail.com>
2024-04-20 19:40:23 -0600

    create a page createUser and delete password in dbconnection

--- actions/dbconnection.php
+++ actions/dbconnection.php
@@ -4,7 +4,7 @@ function dbconnection()
     $servername = "127.0.0.1:3307";
     $database = "proyect_ticos_rides";
     $username = "root";
-    $password = "root1234";
+    $password = "";
     $conn = mysqli_connect($servername, $username, $password, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
